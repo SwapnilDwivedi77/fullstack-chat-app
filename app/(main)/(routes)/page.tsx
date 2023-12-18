908a8b04edd222caac9f88cae013d06db49c20ce
dwivedi-07e <swapnil.dwivedi@ck12.org>
2023-12-18 13:30:06 +0530

    feat-Added dark mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 const Home = () => {
     return ( <>
     <UserButton afterSignOutUrl="/sign-in"/>
+    <ModeToggle/>
     </> );
 }
  
