7ce66fad9ff3eba2075a90e06b545e3a318e073d
dwivedi-07e <swapnil.dwivedi@ck12.org>
2023-12-29 00:55:51 +0530

    [feat] : manage members functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -69,6 +69,7 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           )}
           {isAdmin && (
             <DropdownMenuItem
+            onClick={()=> onOpen("members",{server})}
               className="
                     px-3 py-2 text-sm cursor-pointer  "
             >
