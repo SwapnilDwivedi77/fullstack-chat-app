f2410feb3192af35360fd57efcafd47ed569588f
dwivedi-07e <swapnil.dwivedi@ck12.org>
2023-12-29 09:12:27 +0530

    [feat]: Added functionality to manage members

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -79,6 +79,7 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           )}
           {isModerator && (
             <DropdownMenuItem
+            onClick={()=> onOpen("createChannel")}
               className="
                     px-3 py-2 text-sm cursor-pointer  "
             >
