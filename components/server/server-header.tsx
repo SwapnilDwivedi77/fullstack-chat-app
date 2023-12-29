9a409b795cd6e9d1254b4080f260d05a6b0e6975
dwivedi-07e <swapnil.dwivedi@ck12.org>
2023-12-29 09:33:58 +0530

    [feat]: Added leave server function

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -99,6 +99,7 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           )}
           {!isAdmin && (
             <DropdownMenuItem
+            onClick={()=> onOpen("leaveServer",{server})}
               className="
                     px-3 py-2 text-sm cursor-pointer "
             >
